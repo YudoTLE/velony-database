b8e1c1553eb70c5fecf34d346139fc78be1b8891
YudoTLE <yudojago@gmail.com>
2025-12-14 18:48:03 +0700

    feat: split index on messages table into separate indexes for conversation_id, id and conversation_id, version

--- flyway/sql/V37__adjust_indexes_for_message_table.sql
+++ flyway/sql/V37__adjust_indexes_for_message_table.sql
@@ -2,6 +2,7 @@ BEGIN;
 
 DROP INDEX IF EXISTS public.idx_messages_version_id;
 
-CREATE INDEX idx_messages_conversation_id_id_version ON public.messages(conversation_id, id, version);
+CREATE INDEX idx_messages_conversation_id_id ON public.messages(conversation_id, id);
+CREATE INDEX idx_messages_conversation_id_version ON public.messages(conversation_id, version);
 
 COMMIT;
